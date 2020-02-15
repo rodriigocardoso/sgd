59aefeefa0edb4e0aa85b8a9eb32ca44897c3f57
rodriigocardoso <61062581+rodriigocardoso@users.noreply.github.com>
2020-02-14 21:55:24 -0300

    Add files via upload

---
+++ Scripts/home.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Home</title>
+<script type="text/javascript" src="jquery-1.5.2.min.js"></script>
+<script type="text/javascript">
+$(document).ready(function(){  
+   $('#ajuda').hide();
+      $('a#abreAjuda').click(function(){
+	    $('#ajuda').show(); 
+     });
+
+      $('a#fechar').click(function(){
+    		  $('#ajuda').hide();
+     }); 
+});
+</script>
+</head>
+<body>
+    <style type="text/css">
+
+#ajuda{
+
+position: fixed;
+    width: 200px;
+    height: 200px;
+    left: 50%;
+    top: 50%;
+    margin: -100px -100px 0 0;
+
+}
+
+</style>
+    
+    
+<p><a id="abreAjuda" href="#">Ajuda</a></p>
+<div id="ajuda">
+     <a id="fechar" href="#">Fechar [x]</a> 
+      <?php include 'ajuda.php'; ?>
+</div>
+<p> </p>
+<p> </p>
+<p> </p>
+<p> </p>
+<p>Página principal</p>
+</body>
